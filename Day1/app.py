69d337ecd9cccb55c8162c8f5529d77090e4314f
Zeeshan Alam <zeeshantechnical9128@gmail.com>
2021-12-04 20:57:13 +0530

    Complete Day1

---
+++ Day1/app.py
@@ -0,0 +1,10 @@
+with open("input.txt", "r") as file:
+    count = 0
+    f = file.readlines()
+    for i in range(0 , len(f)-1):
+        if f[i+1]:
+            a = int(f[i])
+            b = int(f[i+1])
+            if int(f[i]) < int(f[i+1]):
+                count += 1
+    print(count)
